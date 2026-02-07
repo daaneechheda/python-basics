6742723bd41ac707b4af54baff381ab314eaab14
daaneechheda <daaneechheda@gmail.com>
2026-02-07 14:54:23 +0530

    Python basics, datatypes and conditions

---
+++ day1_basics.py
@@ -0,0 +1,33 @@
+#basics
+print("Daanee")
+print(100)
+city = "mumbai"
+print(city)
+city = "delhi"
+print(city)
+
+
+#datatypes
+age = 5
+height = 5.5
+name = "Daanee"
+is_Learning = True
+
+print(age)
+print(height)
+print(name)
+print(is_Learning)
+
+print(type(age))
+print(type(height))
+print(type(name))
+print(type(is_Learning))
+
+
+#conditions
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("You can vote")
+else:
+    print ("You cannot vote")
+print(type(age))
